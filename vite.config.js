7f4b40af3c1c26f5ec3d1cc69bd65068ff605ce3
sabyasachi40 <b322040@iiit-bh.ac.in>
2025-08-28 23:24:04 +0530

    vite file updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    // ADDED: Configuration for CodeSandbox
+    hmr: {
+      clientPort: 443,
+    },
+    allowedHosts: ['.csb.app'],
   },
   build: {
     outDir: 'dist',
